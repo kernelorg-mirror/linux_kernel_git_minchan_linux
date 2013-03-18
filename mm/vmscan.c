b49f48d4f2f0f4c6565bc947224b7ec0a6287593
Minchan Kim <minchan@kernel.org>
2013-03-18 09:53:45 +0900

    mm: Enhance per process reclaim
    Some pages could be shared by several processes. (ex, libc)
    In case of that, it's too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesn't handle non-linear mapping on ramfs because
    it's very time-consuming and doesn't make sure of reclaiming and
    not common.
    
    Signed-off-by: Sangseok Lee <sangseok.lee@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -92,6 +92,13 @@ struct scan_control {
 	 * are scanned.
 	 */
 	nodemask_t	*nodemask;
+
+	/*
+	 * Reclaim pages from a vma. If the page is shared by other tasks
+	 * it is zapped from a vma without reclaim so it ends up remaining
+	 * on memory until last task zap it.
+	 */
+	struct vm_area_struct *vma;
 };
 
 #define lru_to_page(_head) (list_entry((_head)->prev, struct page, lru))
@@ -793,7 +800,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		 * processes. Try to unmap it here.
 		 */
 		if (page_mapped(page) && mapping) {
-			switch (try_to_unmap(page, ttu_flags)) {
+			switch (try_to_unmap(page, ttu_flags, sc->vma)) {
 			case SWAP_FAIL:
 				goto activate_locked;
 			case SWAP_AGAIN:
@@ -1000,13 +1007,15 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 }
 
 #ifdef CONFIG_PROCESS_RECLAIM
-unsigned long reclaim_pages_from_list(struct list_head *page_list)
+unsigned long reclaim_pages_from_list(struct list_head *page_list,
+					struct vm_area_struct *vma)
 {
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
 		.priority = DEF_PRIORITY,
 		.may_unmap = 1,
 		.may_swap = 1,
+		.vma = vma,
 	};
 
 	unsigned long nr_reclaimed;
